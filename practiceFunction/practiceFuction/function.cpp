d4d521a3a1190514a2da67d8435b5e21547a5f5e
EncyH <dupyoh.id@gmail.com>
2020-03-27 13:07:01 +0900

    [add] practiceFunction

---
+++ practiceFunction/practiceFuction/function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void print_square(int arg);
+
+int main()
+{
+	int i;
+	
+	std::cout << "제곱할 수 ? :";
+	std::cin >> i;
+
+	print_square(i);
+
+	system("pause");
+
+	return 0;
+}
+
+void print_square(int arg)
+{
+	std::cout << "전달된 인자 : " << arg * arg << std::endl;
+}
