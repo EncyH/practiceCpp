2657f8c90897a68dae7d40792f32896611504dc5
EncyH <dupyoh.id@gmail.com>
2020-04-01 14:32:59 +0900

    [edit] edit a pracatice_opp

--- practice_opp/practice_opp.cpp
+++ practice_opp/practice_opp.cpp
@@ -16,21 +16,43 @@ public:
 	void ShowDate();
 
 private:
-	int year_;
-	int month_;
-	int day_;
+	int year_, increase_year_;
+	int month_, increase_month_;
+	int day_, increase_day_;
 };
 void Date::SetDate(int year, int month, int date) {
 	year_ = year;
 	month_ = month;
 	day_ = date;
+
+	AddDay(increase_day_);
+	AddMonth(increase_month_);
+	AddYear(increase_year_);
+	
 }
 void Date::AddDay(int _increase) {
 	day_ += _increase;
+	while (day_ > 31) {
+		if (month_ == 1 || 3 || 5 || 7 || 8 || 10 || 12) {
+			day_ -= 31;
+			month_++;
+		}
+		if (month_ == 4 || 6 || 9 || 11) {
+			day_ -= 30;
+			month_++;
+		}
+		else {
+			day_ -= 28;
+			month_++;
+		}
+	}
 }
-
 void Date::AddMonth(int _increase) {
 	month_ += _increase;
+	while (month_ > 12) {
+		month_ -= 12;
+		year_++;
+	}
 }
 void Date::AddYear(int _increase) {
 	year_ += _increase;
@@ -39,13 +61,27 @@ void Date::ShowDate() {
 	std::cout << year_ << month_ << day_ << std::endl;
 }
 int main() {
-	int year;
-	int month;
-	int day;
+	int year, increase_year;
+	int month, increase_month;
+	int day, increase_day;
 	Date Callender;
 
-	std::cout << "다음의 형식에 맞게 날짜를 설정해주세요. (ex. yyyymmdd) " << std::endl;
-	std::cin >> year >> month >> day;
+	std::cout << "설정할 연도를 입력해 주세요. : ";
+	std::cin >> year;
+	std::cout << "설정할 월을 입력해 주세요. : ";
+	std::cin >> month;
+	std::cout << "설정할 일을 입력해 주세요. : ";
+	std::cin >> day;
+
+	std::cout << "설정한 연도에서 더하고 싶은 년수를 입력해주세요. : ";
+	std::cin >> increase_year;
+	//Callender.AddYear(increase_year);
+	std::cout << "설정한 연도에서 더하고 싶은 월수를 입력해주세요. : ";
+	std::cin >> increase_month;
+	//Callender.AddMonth(increase_month);
+	std::cout << "설정한 연도에서 더하고 싶은 일수를 입력해주세요. : ";
+	std::cin >> increase_day;
+	//Callender.AddDay(increase_day);
 	
 	Callender.SetDate(year, month, day);
 	Callender.ShowDate();
